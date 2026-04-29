a5f72c24fbd3ec84e5ae0d9b44e904b6c2ab171c
mhmood88hz-cloud <mhmood88hz@gmail.com>
2026-04-29 09:42:49 +0200

    Update animals web generator

---
+++ animals_web_generator.py
@@ -0,0 +1,22 @@
+import json
+
+from pydantic_core.core_schema import dataclass_args_schema
+
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+
+animals_data = load_data("animals_data.json")
+for i in range(len(animals_data)):
+  print(f"Name: {animals_data[i]["name"]}")
+  #print(f"{animals_data[i]["taxonomy"]}")
+  #print(f"{animals_data[i]["locations"]}")
+  print(f"Diet: {animals_data[i]["characteristics"]['diet']}")
+  print(f"Location: {animals_data[i]["locations"]}")
+  try:
+    print(f"Type: {animals_data[i]["characteristics"]["type"]}")
+  except KeyError:
+    pass
