ae037587d9baa04bd243ab9d7f914d8dbada496d
mhmood88hz-cloud <mhmood88hz@gmail.com>
2026-04-29 22:41:12 +0200

    Tierdaten in HTML einfügen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,6 +1,7 @@
 import json
 
 from pydantic_core.core_schema import dataclass_args_schema
+from pyparsing import html_comment
 
 
 def load_data(file_path):
@@ -10,14 +11,17 @@ def load_data(file_path):
 
 
 animals_data = load_data("animals_data.json")
+output = ""
 for i in range(len(animals_data)):
-  print(f"Name: {animals_data[i]["name"]}")
-  #print(f"{animals_data[i]["taxonomy"]}")
-  #print(f"{animals_data[i]["locations"]}")
-  print(f"Diet: {animals_data[i]["characteristics"]['diet']}")
-  print(f"Location: {animals_data[i]["locations"]}")
+  output += f"Name: {animals_data[i]["name"]}\n"
+  output += f"Diet: {animals_data[i]["characteristics"]['diet']}\n"
+  output += f"Location: {animals_data[i]["locations"]}\n"
   try:
-    print(f"Type: {animals_data[i]["characteristics"]["type"]}")
+    output += f"Type: {animals_data[i]["characteristics"]["type"]}\n"
   except KeyError:
-
     pass
+with open("animals_template.html", "r") as file:
+  html_comment = file.read()
+new_html = html_comment.replace("{{animals_data}}", output)
+with open("animals.html", "w") as file:
+  file.write(new_html)
