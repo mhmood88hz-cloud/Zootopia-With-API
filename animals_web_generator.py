4b91a6a967a790917c9af749b3121c81ab37c594
mhmood88hz-cloud <mhmood88hz@gmail.com>
2026-04-30 10:29:55 +0200

    erstelle Karten-Element für jedes Tier

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,13 +13,15 @@ def load_data(file_path):
 animals_data = load_data("animals_data.json")
 output = ""
 for i in range(len(animals_data)):
+  output += '<li class="cards__item">'
   output += f"Name: {animals_data[i]["name"]}\n"
-  output += f"Diet: {animals_data[i]["characteristics"]['diet']}\n"
-  output += f"Location: {animals_data[i]["locations"]}\n"
+  output += f"Diet: {animals_data[i]["characteristics"]['diet']}<br/>\n"
+  output += f"Location: {", ".join(animals_data[i]["locations"])}<br/>\n"
   try:
-    output += f"Type: {animals_data[i]["characteristics"]["type"]}\n"
+    output += f"Type: {animals_data[i]["characteristics"]["type"]}<br/>\n"
   except KeyError:
     pass
+  output += "</i>"
 with open("animals_template.html", "r") as file:
   html_comment = file.read()
 new_html = html_comment.replace("{{animals_data}}", output)
