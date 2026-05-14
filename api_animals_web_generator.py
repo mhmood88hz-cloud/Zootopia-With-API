70a5408e07ba1b8134cb197a6a65ea122879b718
mhmood88hz-cloud <mhmood88hz@gmail.com>
2026-05-14 12:53:12 +0200

    baisis  Dateien hochgelad

--- api_animals_web_generator.py
+++ api_animals_web_generator.py
@@ -8,7 +8,7 @@ def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
-
+#MM
 
 animals_data = load_data("animals_data.json")
 def serialize_animal():
